7795ff93dbdb211710e1bbc6eefd74b73fabf49d
Samir78611 <qureshisamar351@gmail.com>
2023-12-27 09:10:45 +0530

    layout add in offer

--- resources/views/offer.blade.php
+++ resources/views/offer.blade.php
@@ -1,32 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>offer</title>
-</head>
+@extends('layouts.admin')
+@section('content')
 <body>
 
-    @if(Session::has('success'))
-    <h1 style="color:#006600">{{Session::get('success')}}</h1>
-    @endif
+@if(Session::has('success'))
+<h1 style="color:#006600">{{Session::get('success')}}</h1>
+@endif
 
-    @if(Session::has('fail'))
-    <h1 style="color:#ff0000">{{Session::get('fail')}}</h1>
-    @endif
+@if(Session::has('fail'))
+<h1 style="color:#ff0000">{{Session::get('fail')}}</h1>
+@endif
 
-    <h1>Send Offer to Customer</h1>
-    <form action="{{url('users_offer')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <label for="">Title of this offer</label><br>
-        <input type="text" id="title" name="title" value=""><br>
+<h1>Send Offer to Customer</h1>
+<form action="{{url('users_offer')}}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="">Title of this offer</label><br>
+    <input type="text" id="title" name="title" value=""><br>
 
-        <label for="">Description of this offer</label><br>
-        <textarea name="description" id="description" cols="30" rows="10"></textarea>
-        <br>
+    <label for="">Description of this offer</label><br>
+    <textarea name="description" id="description" cols="30" rows="10"></textarea>
+    <br>
 
-        <input type="reset">
-        <input type="submit">
-    </form>
+    <input type="reset">
+    <input type="submit">
+</form>
 </body>
-</html>
+@stop
